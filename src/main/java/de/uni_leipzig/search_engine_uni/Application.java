001b4c85c048059f05be0ab8e19b8d0c1fc09605
Maik Fröbe <maik44froebe@web.de>
2017-10-22 13:01:10 +0200

    Add Spring-Boot-Startup-Application and a component to search within the created lucene index

---
+++ src/main/java/de/uni_leipzig/search_engine_uni/Application.java
@@ -0,0 +1,13 @@
+package de.uni_leipzig.search_engine_uni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ src/main/java/de/uni_leipzig/search_engine_uni/lucene/SearcherComponent.java
@@ -0,0 +1,48 @@
+package de.uni_leipzig.search_engine_uni.lucene;
+
+import java.nio.file.Paths;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.FSDirectory;
+import org.springframework.stereotype.Component;
+
+import lombok.SneakyThrows;
+
+@Component
+public class SearcherComponent
+{
+	public static final int HITS_PER_PAGE = 10;
+	
+	private final IndexSearcher searcher;
+	
+	private final QueryParser queryParser;
+	
+	@SneakyThrows
+	public SearcherComponent()
+	{
+		IndexReader indexReader = DirectoryReader.open(FSDirectory.open(Paths.get("lucene_index")));
+		searcher = new IndexSearcher(indexReader);
+		queryParser = new MultiFieldQueryParser(new String[] {"title", "content"}, 
+					new StandardAnalyzer());
+		queryParser.setFuzzyMinSim(0.1f);
+	}
+	
+	@SneakyThrows
+	public TopDocs search(String query, int n)
+	{
+		return searcher.search(queryParser.parse(query), n);
+	}
+	
+	@SneakyThrows
+	public Document doc(int docID)
+	{
+		return searcher.doc(docID);
+	}
+}
