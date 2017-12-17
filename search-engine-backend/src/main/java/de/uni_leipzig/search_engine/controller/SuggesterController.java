9e87b280324696d047bb4ad4b0c7481017acbbec
Danilo <Danilo@WORKZONE>
2017-12-17 11:27:48 +0100

    Edits.

--- search-engine-backend/src/main/java/de/uni_leipzig/search_engine/controller/SuggesterController.java
+++ search-engine-backend/src/main/java/de/uni_leipzig/search_engine/controller/SuggesterController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SuggesterController {
@@ -17,28 +16,12 @@ public class SuggesterController {
     public SuggesterController() {
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView getPages() {
-
-        ModelAndView model = new ModelAndView("example");
-        return model;
-
-    }
-
     @RequestMapping(value = "/getQueries", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
     @ResponseBody
     public List<String> getQueries(@RequestParam String search) {
         
         return searchDB.search(search);
 
-    }
-    
-    @RequestMapping(value = "/saveDB", method = RequestMethod.POST)
-    @ResponseBody
-    public void saveDB(@RequestParam Boolean save) {
-        
-        if (save) searchDB.saveDB();
-        
     }
     
     @RequestMapping(value = "/addQuery", method = RequestMethod.POST, produces = "application/json; charset=utf-8")
