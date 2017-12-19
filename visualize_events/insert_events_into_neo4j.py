bc3962bca2f9ba7894fde9009f23460f2e657c6c
Maik Fröbe <maik44froebe@web.de>
2017-12-19 07:44:01 +0100

    fix visualization script

--- visualize_events/insert_events_into_neo4j.py
+++ visualize_events/insert_events_into_neo4j.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import json
+from threading import Thread
 from kafka import KafkaConsumer
 from neo4j.v1 import GraphDatabase
 
@@ -11,19 +12,40 @@ def executeCypher(cypherStatement):
 		with session.begin_transaction() as tx:
 			tx.run(cypherStatement)
 
-executeCypher("MATCH (n) DETACH DELETE n;")
 
-for msg in KafkaConsumer("uni_search", auto_offset_reset='earliest'):
-	print("Handle Message-Offset: "+ str(msg.offset))
-	event = json.loads(msg.value.decode("utf-8"))
-	request = event.get("request")
+def webEventsConsumer():
+	for msg in KafkaConsumer("web_events", auto_offset_reset='earliest'):
+		print("web_events: Handle Message-Offset: "+ str(msg.offset))
+		event = json.loads(msg.value.decode("utf-8"))
+		request = event.get("request")
+	
+		executeCypher("MERGE (client:client {clientId: '"+ event.get("clientId") +"'}) "+
+			"MERGE (address:ip {address: '"+ request.get("remoteAddr") +"'}) "+
+			"MERGE (userAgent:userAgent {name: '"+ request.get("headers").get("user-agent") +"'}) "+
+			"MERGE (client)-[userAgentRelationship:executed_request_from]->(userAgent) "+
+				"ON CREATE SET userAgentRelationship.count = 1 ON MATCH SET userAgentRelationship.count = userAgentRelationship.count +1 "+
+			"MERGE (client)-[request:executed_request_from]->(address) "+
+				"ON CREATE SET request.count = 1 ON MATCH SET request.count = request.count +1;")
 
-	if "query" in event and "resultPage" in event:
+
+def searchEventsConsumer():
+	for msg in KafkaConsumer("search_events", auto_offset_reset='earliest'):
+		print("search_events: Handle Message-Offset: "+ str(msg.offset))
+		event = json.loads(msg.value.decode("utf-8"))
+		request = event.get("request")
+
 		executeCypher("MERGE (client:client {clientId: '"+ event.get("clientId") +"'}) "+
 			"MERGE (query:query {query: '"+ event.get("query") +"', resultPage:"+ str(event.get("resultPage")) +"}) "+
 			"MERGE (client)-[r:executedQuery]->(query) "+
 				"ON CREATE SET r.count = 1 ON MATCH SET r.count = r.count +1;");
-	elif "resultId" in event and "query" in event and "searchPage" in event:
+
+
+def selectSearchResultEventsConsumer():
+	for msg in KafkaConsumer("select_search_result_events", auto_offset_reset='earliest'):
+		print("select_search_result_events: Handle Message-Offset: "+ str(msg.offset))
+		event = json.loads(msg.value.decode("utf-8"))
+		request = event.get("request")
+
 		executeCypher("MERGE (client:client {clientId: '"+ event.get("clientId") +"'}) "+
 			"MERGE (query:query {query: '"+ event.get("query") +"', resultPage: "+ str(event.get("searchPage")) +"}) "+
 			"MERGE (clickedResult:clicked {documentId: "+ str(event.get("resultId")) +", "+
@@ -32,10 +54,17 @@ for msg in KafkaConsumer("uni_search", auto_offset_reset='earliest'):
 				"ON CREATE SET clicked.count = 1 ON MATCH SET clicked.count = clicked.count +1 "+
 			"MERGE (clickedResult)-[:from]->(query);")
 
-	executeCypher("MERGE (client:client {clientId: '"+ event.get("clientId") +"'}) "+
-		"MERGE (address:ip {address: '"+ request.get("remoteAddr") +"'}) "+
-		"MERGE (userAgent:userAgent {name: '"+ request.get("headers").get("user-agent") +"'}) "+
-		"MERGE (client)-[userAgentRelationship:executed_request_from]->(userAgent) "+
-			"ON CREATE SET userAgentRelationship.count = 1 ON MATCH SET userAgentRelationship.count = userAgentRelationship.count +1 "+
-		"MERGE (client)-[request:executed_request_from]->(address) "+
-			"ON CREATE SET request.count = 1 ON MATCH SET request.count = request.count +1;")
+
+executeCypher("MATCH (n) DETACH DELETE n;")
+
+webEventsConsumerThread = Thread(target=webEventsConsumer, args=())
+searchEventsConsumerThread = Thread(target=searchEventsConsumer, args=())
+selectSearchResultEventsConsumerThread = Thread(target=selectSearchResultEventsConsumer, args=())
+
+webEventsConsumerThread.start()
+searchEventsConsumerThread.start()
+selectSearchResultEventsConsumerThread.start()
+
+webEventsConsumerThread.join()
+searchEventsConsumerThread.join()
+selectSearchResultEventsConsumerThread.join()
